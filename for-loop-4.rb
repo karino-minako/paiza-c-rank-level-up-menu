77e59e127b75ca36e1cb27dd17da5c121953242f
karino-minako <irikiiomo11@gmail.com>
2020-08-30 17:54:33 +0900

    4 questions finished

---
+++ for-loop-4.rb
@@ -0,0 +1,19 @@
+# 各 c_i （1 ≤ i ≤ m） について、各 S_j （1 ≤ j ≤ n） に c_i が出現するかをそれぞれ調べ、
+# 出現する場合は "YES" を、そうでない場合には "NO" を、そのつど出力してください。	# 出現する場合は "YES" を、そうでない場合には "NO" を、そのつど出力してください。
+
+
+m = gets.to_i
+c = m.times.map{gets.chomp}
+n = gets.to_i
+s = n.times.map{gets.chomp}
+
+for i in 1..m do
+  for e in 1..n do
+    confirm = s[e-1].include?(c[i-1])
+    if confirm == true
+      puts "YES"
+    else
+      puts "NO"
+    end
+  end
+end
---
+++ sort-1.rb
@@ -0,0 +1,5 @@
+# n 個の数 a_1, … , a_n が与えられます。与えられた数を小さい順に改行区切りで出力してください。
+
+n = gets.to_i
+a = n.times.map{gets.to_i}
+puts a.sort
