0e7ca8079e1b2b6e96a007b45599acea9ed4b4d7
karino-minako <irikiiomo11@gmail.com>
2020-09-02 10:16:24 +0900

    [Review]Standard-input/output

---
+++ [Review]Standard input:output-5.rb
@@ -0,0 +1,8 @@
+整数 n が与えられるので paiza と n 回、半角スペース区切りで出力してください。
+
+n = gets.to_i
+a = n - 1
+a.times do
+  print "paiza "
+end
+print "paiza"
---
+++ [Review]Standard-input:output-4.rb
@@ -0,0 +1,5 @@
+# 整数 n と n 個の数 a_1, ..., a_n が改行区切りで与えられます。与えられた a_1, ..., a_n の中で最も大きい数を出力してください。
+
+n = gets.to_i
+a = n.times.map{gets.to_i}
+puts a.max
